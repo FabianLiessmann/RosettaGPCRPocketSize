e5cc034be85b33426751d7ed54b95aa720143cf1
Fabian Liessmann <fabian.liessmann@medizin.uni-leipzig.de>
2023-01-12 14:55:22 +0100

    Add files via upload

---
+++ ADRB1_example/Step2_Generate_CST/copy_cst_and_xml.sh
@@ -0,0 +1,11 @@
+cp *.cst ../Step3_RosettaGPCR/input/
+sed "s/constraint_number/ADRB1_1.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm1.xml
+sed "s/constraint_number/ADRB1_2.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm2.xml
+sed "s/constraint_number/ADRB1_3.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm3.xml
+sed "s/constraint_number/ADRB1_4.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm4.xml
+sed "s/constraint_number/ADRB1_5.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm5.xml
+sed "s/constraint_number/ADRB1_6.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm6.xml
+sed "s/constraint_number/ADRB1_7.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm7.xml
+sed "s/constraint_number/ADRB1_8.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm8.xml
+sed "s/constraint_number/ADRB1_9.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm9.xml
+sed "s/constraint_number/ADRB1_10.cst/g" ../Step3_RosettaGPCR/input/rosetta_cm.xml >>../Step3_RosettaGPCR/input/rosetta_cm10.xml
